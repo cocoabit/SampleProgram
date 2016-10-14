f16d94d27a8e63843f0db468fc840c412382269d
wami <wamiwami@live.jp>
2016-10-15 08:09:15 +0900

    上限値の変更

--- CocoaBit.cpp
+++ CocoaBit.cpp
@@ -1,9 +1,11 @@
 #include "CocoaBit.h"
 
 void CocoaBit_lib::pwmWrite(int pin, int value)
-{
+{	
 	analogWriteFreq(4000);
-
+	analogWriteRange(990);
+	if (value > 99)value = 99;
+	if (0 > value)value = 0;
 	analogWrite(pin, value*10);
 }
 CocoaBit_lib CocoaBit;
